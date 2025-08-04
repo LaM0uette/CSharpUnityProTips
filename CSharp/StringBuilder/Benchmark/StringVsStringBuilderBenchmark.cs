dcf85d0c1fee3bcb68e9325576d0c3fdbbe488ab
LaM0uette <LaM0uette.dev@gmail.com>
2025-08-04 19:15:10 +0200

    add StringBuilder test

---
+++ CSharp/StringBuilder/Benchmark/StringVsStringBuilderBenchmark.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using BenchmarkDotNet.Attributes;
+
+[MemoryDiagnoser, ShortRunJob]
+public class StringVsStringBuilderBenchmark
+{
+    private const int N = 100000;
+
+    [Benchmark]
+    public string Concat()
+    {
+        string result = string.Empty;
+        
+        for (int i = 0; i < N; i++)
+        {
+            result += 'A';
+        }
+        
+        return result;
+    }
+
+    [Benchmark]
+    public string StringBuilder()
+    {
+        StringBuilder stringBuilder = new();
+        
+        for (int i = 0; i < N; i++)
+        {
+            stringBuilder.Append('A');
+        }
+        
+        return stringBuilder.ToString();
+    }
+}
---
+++ CSharp/StringBuilder/Program.cs
@@ -0,0 +1,9 @@
+﻿using BenchmarkDotNet.Running;
+
+class Program
+{
+    private static void Main()
+    {
+        BenchmarkRunner.Run<StringVsStringBuilderBenchmark>();
+    }
+}
